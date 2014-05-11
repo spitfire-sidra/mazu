1ad2a350b0eca02bad417e8db80f482201a4e938
spitfire-sidra <spitfire.sidra@gmail.com>
2014-05-11 21:13:12 +0800

    Added semantic-ui

--- mazu/urls.py
+++ mazu/urls.py
@@ -5,13 +5,11 @@ from django.conf.urls import url
 
 from django.contrib import admin
 
+
 admin.autodiscover()
 
-urlpatterns = patterns('',
-                       # Examples:
-                       # url(r'^$', 'mazu.views.home', name='home'),
-                       # url(r'^blog/', include('blog.urls')),
-
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^malware/', include('malware.urls')),
-                       )
+urlpatterns = patterns(
+    '',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^malware/', include('malware.urls')),
+)
