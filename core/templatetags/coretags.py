f53d254da9c5013dc3d4240ee3c8436937f77b24
spitfire-sidra <spitfire.sidra@gmail.com>
2014-06-06 21:40:10 +0800

    Added an active tag and an url

---
+++ core/templatetags/coretags.py
@@ -0,0 +1,14 @@
+#! -*- utf-8 -*-
+import re
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag
+def active(request, pattern):
+    if re.search(pattern, request.path):
+        return 'active'
+    return ''
