0d10f4bc12a94ccf42f5157aeb0592ea913a352b
spitfire-sidra <spitfire.sidra@gmail.com>
2014-07-16 11:34:00 +0800

    add a celery task for widget app

---
+++ widget/tasks.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import pkgutil
+
+from core.modules import Widget
+from celery import shared_task
+
+
+@shared_task
+def exec_widget_modules():
+    mod_names = [n for _, n, _ in pkgutil.iter_modules(['widget/modules'])]
+    for name in mod_names:
+        imp = 'widget.modules.{}'.format(name)
+        try:
+            # import modules dynamically
+            modules = __import__(imp, globals(), locals(), ['dummy'], -1)
+        except Exception as e:
+            print 'Can not import {}'.format(import_path)
+
+    for widget in Widget.__subclasses__():
+        widget().run()
