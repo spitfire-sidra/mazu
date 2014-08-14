a6450462c18d7e73f29a432e0477405747f7c46a
spitfire-sidra <spitfire.sidra@gmail.com>
2014-08-15 00:03:30 +0800

    add test cases of authkey

--- authkey/tests.py
+++ authkey/tests.py
@@ -1,4 +1,58 @@
-# -*- coding: utf-8 -*-
+# -*- cofing: utf-8 -*-
+import random
+
+from django.test import Client
 from django.test import TestCase
 
-# Create your tests here.
+from django.core.urlresolvers import reverse_lazy
+
+from core.tests import random_string
+
+from .models import AuthKey
+
+
+class AuthKeyTestCase(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self._random_data()
+
+    def _random_data(self):
+        self.authkey = {
+            'ident': random_string(),
+            'secret': random_string(),
+            'pubchans': random_string(),
+            'subchans': random_string()
+        }
+
+    def _create(self):
+        AuthKey(**self.authkey).save()
+
+    def test_list_view(self):
+        self._create()
+        response = self.client.get(reverse_lazy('authkey_list'))
+        authkeys = AuthKey.objects.all()
+        for c in response.context['authkeys']:
+            self.assertIn(c, authkeys)
+
+    def test_can_create(self):
+        expected_count = AuthKey.objects.all().count() + 1
+        self.client.post(reverse_lazy('authkey_create'), self.authkey)
+        count = AuthKey.objects.all().count()
+        self.assertEqual(count, expected_count)
+
+    def test_can_update(self):
+        self._create()
+        pk = AuthKey.objects.get(ident=self.authkey['ident']).id
+        self._random_data()
+        self.client.post(reverse_lazy('authkey_update', args=[pk]), self.authkey)
+        updated_authkey = AuthKey.objects.get(id=pk)
+        self.assertEqual(updated_authkey.ident, self.authkey['ident'])
+
+    def test_can_delete(self):
+        self._create()
+        expected_count = AuthKey.objects.all().count() - 1
+        pk = AuthKey.objects.get(ident=self.authkey['ident']).id
+        self.client.post(reverse_lazy('authkey_delete', args=[pk]))
+        count = AuthKey.objects.all().count()
+        self.assertEqual(count, expected_count)
