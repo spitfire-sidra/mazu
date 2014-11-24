a5de383283c7a8d1027daf9e3b7e7e709b0d1928
spitfire-sidra <spitfire.sidra@gmail.com>
2014-11-24 21:16:57 +0800

    move folder static/ into core/

--- settings/local.example.py
+++ settings/local.example.py
@@ -103,7 +103,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'core', 'static'),
 )
 
 STATIC_URL = '/static/'
