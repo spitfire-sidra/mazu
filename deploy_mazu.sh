05d0ddacc321d67c55bcf6906d329b07c7c6841c
deanboole <xspiritualx@gmail.com>
2014-11-27 16:09:13 +0800

    add mongodb installation (closes #5)

--- deploy_mazu.sh
+++ deploy_mazu.sh
@@ -32,7 +32,7 @@ echo "  > Installing PREREQUISITES..."
 echo "  > Updating package soruce..."
 sudo apt-get update &> /dev/null
 echo "  > Installing python-dev libxml2-dev libxslt-dev..."
-sudo apt-get -y install python-dev libxml2-dev libxslt-dev &> /dev/null
+sudo apt-get -y install python-dev libxml2-dev libxslt-dev mongodb&> /dev/null
 echo "  > Installing packages in requirements..."
 sudo pip install -r requirements.txt &> /dev/null
 
@@ -47,6 +47,8 @@ PRO_GEN=$(/usr/bin/python -c 'import random; print "".join([random.choice("abcde
 # replace key in settings/production.py
 sed -i s/yoursecretkey/$PRO_GEN/g settings/production.py 
 
+echo "  > Creating superuser for django..."
+./manage.py createsuperuser
 
 echo -e "  > Your key is ${YEL}$PRO_GEN${NC}. And is now using in settings/production.py"
 echo -e "${GRN}-= Great, now you're all set to go! =-${NC}"
