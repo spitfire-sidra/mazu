28c64ef53eab3d95911b2fa1b1b9d7869c94df3f
spitfire-sidra <spitfire.sidra@gmail.com>
2014-12-11 23:02:23 +0800

    [WIP] change samples/models.py for saving for sample attributes

--- samples/models.py
+++ samples/models.py
@@ -1,9 +1,53 @@
 # -*- coding: utf-8 -*-
 from django.db import models
+from django.core.validators import URLValidator
 from django.core.urlresolvers import reverse_lazy
 from django.template.defaultfilters import slugify
 
 from core.models import TimeStampedModel
+
+
+class Filename(TimeStampedModel):
+
+    """
+    This models saves filename. A sample might have various filename,
+    a filename might map to samples.
+    """
+
+    name = models.CharField(max_length=255)
+    user = models.ForeignKey('auth.User')
+
+
+class Description(TimeStampedModel):
+
+    """
+    This models saves descriptions of a sample. A sample can have multiple
+    descriptions, and a description might map to samples.
+    """
+
+    text = models.TextField()
+    user = models.ForeignKey('auth.User')
+
+
+class Link(TimeStampedModel):
+
+    """
+    This model saves sample's links. Download links, report links or
+    related links.
+    """
+
+    KIND_CHOICES = (
+        (0, 'Download Link'),
+        (1, 'Report Link'),
+        (2, 'Related Link'),
+    )
+
+    url = models.TextField(validators=[URLValidator()])
+    kind = models.IntegerField(max_length=2, choices=KIND_CHOICES, default=0)
+    user = models.ForeignKey('auth.User')
+
+    class Meta:
+        ordering = ['kind', '-created']
 
 
 class SampleSource(TimeStampedModel):
@@ -49,7 +93,9 @@ class Sample(TimeStampedModel):
     filetype = models.CharField(max_length=255, default='Unknown')
     size = models.IntegerField(default=0)
     crc32 = models.IntegerField(max_length=255)
-    slug = models.SlugField(max_length=128)
+    sources = models.ManyToManyField(SampleSource, null=True, blank=True)
+    filenames = models.ManyToManyField(Filename, null=True, blank=True)
+    descriptions = models.ManyToManyField(Description, null=True, blank=True)
     user = models.ForeignKey('auth.User')
 
     def __unicode__(self):
@@ -58,26 +104,8 @@ class Sample(TimeStampedModel):
     def get_absolute_url(self):
         return reverse_lazy('malware.list')
 
-    def save(self, *args, **kwargs):
-        if not self.id:
-            self.slug = slugify(self.sha256)
-        super(Sample, self).save(*args, **kwargs)
-
     class Meta:
         ordering = ['-created', '-updated']
-
-
-class SampleExtraInfo(TimeStampedModel):
-
-    """
-    SampleExtraInfo model stores extra information about a sample.
-    """
-
-    sample = models.ForeignKey(Sample)
-    filename = models.CharField(max_length=255, null=True, blank=True)
-    sample_source = models.ForeignKey(SampleSource, blank=True, null=True)
-    link = models.TextField(null=True, blank=True)
-    descr = models.TextField(default='', null=True, blank=True)
 
 
 class DownloadLog(TimeStampedModel):
